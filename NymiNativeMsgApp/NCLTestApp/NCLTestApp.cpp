c0b5a00534e11f96b45881d8bd8359ef5a51a20d
Lauren Long <laurenzlong@gmail.com>
2014-04-15 13:44:48 -0400

    edited C++ to determine target of message

--- NymiNativeMsgApp/NCLTestApp/NCLTestApp.cpp
+++ NymiNativeMsgApp/NCLTestApp/NCLTestApp.cpp
@@ -17,10 +17,12 @@ int gAgreementHandle;
 std::vector<NclProvision> gProvisions; // the allowed provisions
 
 
-void sendMsgToChrome (std::string msg){
-	std::stringstream ss;
-	ss << "{\"text\": \"" << msg << "\"}";
-	std::string message = ss.str();
+void sendMsgToChrome (std::string msg, std::string target){
+	//std::stringstream ss;
+	//ss << "{\"text\": \"" << msg << "\"}";
+	//std::string message = ss.str();
+
+	std::string message = "{\"target\": \"" + target + "\", \"text\": \"" + msg + "\"}";
 
 	unsigned int len = message.length();
     // We need to send the 4 bytes of length information
@@ -34,56 +36,56 @@ void sendMsgToChrome (std::string msg){
 
 void callback(NclEvent event, void* userData){
 
+	std::string led_pattern = "";
     switch(event.type){
         case NCL_EVENT_INIT:
-			sendMsgToChrome("NCL_EVENT_INIT passed to callback");
+			sendMsgToChrome("NCL_EVENT_INIT passed to callback", "console");
             if(event.outcome.success) gNclInitialized=true;
             else exit(-1);
             break;
         case NCL_EVENT_DISCOVERY:
-			sendMsgToChrome("NCL_EVENT_DISCOVERY passed to callback");
-            sendMsgToChrome("Nymi discovered");
-            sendMsgToChrome("Stopping scan");
+			sendMsgToChrome("NCL_EVENT_DISCOVERY passed to callback", "console");
+            sendMsgToChrome("Nymi discovered", "user");
+            sendMsgToChrome("Stopping scan", "console");
             nclStopScan();
-            sendMsgToChrome("Agreeing");
+            sendMsgToChrome("Agreeing", "console");
             nclAgree(event.discovery.nymiHandle);
             break;
         case NCL_EVENT_AGREEMENT:
-			sendMsgToChrome("NCL_EVENT_AGREEMENT passed to callback");
+			sendMsgToChrome("NCL_EVENT_AGREEMENT passed to callback", "console");
             gAgreementHandle=event.agreement.nymiHandle;
-            sendMsgToChrome("Is this: ");
+            sendMsgToChrome("Is this: ", "user");
             for(unsigned i=0; i<NCL_LEDS; ++i){
 				std::string led_char = event.agreement.leds[i]?"1":"0";
-                sendMsgToChrome(led_char);
+                led_pattern = led_pattern + led_char;
 			}
-            sendMsgToChrome(" the correct LED pattern (agree/reject)?");
+			sendMsgToChrome(led_pattern, "user");
+            sendMsgToChrome(" the correct LED pattern (agree/reject)?", "user");
             break;
         case NCL_EVENT_PROVISION:
-			sendMsgToChrome("NCL_EVENT_PROVISION passed to callback");
+			sendMsgToChrome("NCL_EVENT_PROVISION passed to callback", "console");
             gProvisions.push_back(event.provision.provision);
-            sendMsgToChrome("provisioned");
+            sendMsgToChrome("provisioned completed", "user");
             break;
         case NCL_EVENT_DISCONNECTION:
-			sendMsgToChrome("NCL_EVENT_DISCONNECTION passed to callback");
-            sendMsgToChrome("disconnected");
+			sendMsgToChrome("NCL_EVENT_DISCONNECTION passed to callback", "console");
+            sendMsgToChrome("disconnected", "console");
             break;
         case NCL_EVENT_FIND:
-			sendMsgToChrome("NCL_EVENT_FIND passed to callback");
-            sendMsgToChrome("Nymi found");
-            sendMsgToChrome("Stopping scan");
+			sendMsgToChrome("NCL_EVENT_FIND passed to callback", "console");
+            sendMsgToChrome("Nymi found", "console");
+            sendMsgToChrome("Stopping scan", "console");
             nclStopScan();
-            sendMsgToChrome("Validating");
+            sendMsgToChrome("Validating the Nymi...", "user");
             nclValidate(event.find.nymiHandle);
             break;
         case NCL_EVENT_VALIDATION:
-			sendMsgToChrome("NCL_EVENT_VALIDATION passed to callback");
-            sendMsgToChrome("VALIDATED");
-            //kReplyString = "yes";
+			sendMsgToChrome("NCL_EVENT_VALIDATION passed to callback", "console");
+            sendMsgToChrome("VALIDATED", "user");
 			break;
-
 		case NCL_EVENT_SCAN_START:
-			sendMsgToChrome("NCL_EVENT_SCAN_START passed to callback");
-			sendMsgToChrome((event.outcome.success?"success":"failure"));
+			sendMsgToChrome("NCL_EVENT_SCAN_START passed to callback", "console");
+			sendMsgToChrome((event.outcome.success?"success":"failure"), "console");
             break;
         default: break;
     }
@@ -144,10 +146,10 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	std::cout.setf( std::ios_base::unitbuf ); //instead of "<< eof" and "flushall"
 
-	sendMsgToChrome("type a command to native app");
+	sendMsgToChrome("type 'setup' if you are using a new Nymi, or 'authenticate' if you are a returning user.", "user");
 
 	if(!nclInit(callback, NULL, "Chrome", NCL_FALSE, stderr)){
-		sendMsgToChrome("nclInit failed");
+		sendMsgToChrome("The Nymi Communication Library failed to run", "user");
 		return -1;
 	}
 	//sendMsgToChrome("before while loop");
@@ -155,49 +157,49 @@ int _tmain(int argc, _TCHAR* argv[])
 	while(true){
        std::string input = getMsgFromChrome();
 
-	   fileStream<<"got input:"+input+"\n"<<std::flush;
+	   //fileStream<<"got input:"+input+"\n"<<std::flush;
 
-	   sendMsgToChrome("exited getMsgFromChrome()");
+	   //sendMsgToChrome("exited getMsgFromChrome()");
 	   
        if(!gNclInitialized){
-		   fileStream<<"NCL not initialized"<<std::flush;
-           sendMsgToChrome("NCL didn't finished initializing yet!");
+		   //fileStream<<"NCL not initialized"<<std::flush;
+           sendMsgToChrome("NCL didn't finished initializing yet, please wait.", "user");
            continue;
        }
-       else if(input=="\"discover\""){
-		   fileStream<<"got to discover clause"<<std::flush;
-           sendMsgToChrome("starting discovery");
+       else if(input=="\"setup\""){
+		   //fileStream<<"got to discover clause"<<std::flush;
+           sendMsgToChrome("starting discovery", "console");
            NclBool result = nclStartDiscovery();
 		   //sendMsgToChrome(result.str());
-		   sendMsgToChrome(" done discovery");
+		   sendMsgToChrome(" done discovery", "console");
        }
        else if(input=="\"agree\""){
-           sendMsgToChrome("provisioning");
+           sendMsgToChrome("provisioning", "console");
            nclProvision(gAgreementHandle);
-		   sendMsgToChrome("done provisioning");
+		   sendMsgToChrome("done provisioning", "console");
        }
        else if(input=="\"reject\""){
-           sendMsgToChrome("disconnecting");
+           sendMsgToChrome("disconnecting", "console");
            nclDisconnect(gAgreementHandle);
-		   sendMsgToChrome("done discovery");
+		   sendMsgToChrome("done disconnecting", "console");
        }
-       else if(input=="\"find\""){
-           sendMsgToChrome("starting finding");
+       else if(input=="\"authenticate\""){
+           sendMsgToChrome("starting finding", "console");
            nclStartFinding(gProvisions.data(), gProvisions.size());
-		   sendMsgToChrome("done finding");
+		   sendMsgToChrome("done finding", "console");
        }
        else if(input=="\"quit\""){ 
-		   sendMsgToChrome("program quitting");
+		   sendMsgToChrome("program quitting", "console");
 		   break;
 	   }
 	   else{
-		   fileStream<<"got to else clause"<<std::flush;
-		   sendMsgToChrome("invalid input");
+		   //fileStream<<"got to else clause"<<std::flush;
+		   sendMsgToChrome("invalid input, please type 'setup', 'authenticate', or 'quit'", "user");
 		   continue;
 	   }
    }
    //finish
-   sendMsgToChrome("after while loop");
+   //sendMsgToChrome("after while loop");
    nclFinish();
 
 	return 0;
